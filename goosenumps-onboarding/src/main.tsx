28088dbec384d7c064044c6ae34a5d48807edc8d
subhra-io <subhrajeetpandey2001@gmail.com>
2026-05-02 16:16:23 +0530

    feat: add Vercel Analytics
    - Install @vercel/analytics in goosenumps-onboarding
    - Inject <Analytics /> component in main.tsx
    - Tracks page views and web vitals automatically on Vercel

--- goosenumps-onboarding/src/main.tsx
+++ goosenumps-onboarding/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 import App from './App.tsx'
 import { OnboardingProvider } from './context/OnboardingContext.tsx'
@@ -8,6 +9,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <OnboardingProvider>
       <App />
+      <Analytics />
     </OnboardingProvider>
   </StrictMode>,
 )
